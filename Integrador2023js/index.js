ef807fbd58e2f45e30091e94f46cc5045660e76f
AHHAED <126585723+AHHAED@users.noreply.github.com>
2023-05-21 21:54:45 -0300

    Add files via upload

---
+++ Integrador2023js/index.js
@@ -0,0 +1,110 @@
+// Valor de ticket
+const valorTicket = 200;
+
+// Porcentajes de descuento según categoría
+let descuentoEstudiante = 80;
+let descuentoTrainee    = 50;
+let descuentoJunior     = 15;
+
+// Elementos
+let nombre          = document.getElementById("nombre");
+let apellido        = document.getElementById("apellido");
+let mail            = document.getElementById("mail");
+let cantidadTickets = document.getElementById("cantidadTickets");
+let categoria       = document.getElementById("categoriaSelect");
+
+// Estilo de error a los elementos del form
+function quitarClaseError() {
+    let x = document.querySelectorAll(".form-control, .form-select");
+    let i;
+    for (i = 0; i < x.length; i++) {
+        x[i].classList.remove('is-invalid');
+    }
+}
+
+// Total a pagar
+function total_a_pagar() {
+
+    // Ejecuto función para que quite todos los estilos de error en los campos que los tuvieran
+    quitarClaseError();
+
+    // Verificar si lleno los campos, sino se detiene
+    if (nombre.value === "") {
+        alert("Por favor, escribí tu nombre.");
+        nombre.classList.add("is-invalid");
+        nombre.focus();
+        return;
+    }
+
+    if (apellido.value === "" ) {
+        alert("Por favor, escribí tu apellido.");
+        apellido.classList.add("is-invalid");
+        apellido.focus();
+        return;
+    }
+
+    if (mail.value === "") {
+        alert("Por favor, escribí tu email.");
+        mail.classList.add("is-invalid");
+        mail.focus();
+        return;
+    }
+
+    // Correo electrónico es válido o no
+    const emailValido = mail => {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(mail);
+    }
+
+    if (!emailValido(mail.value)) {
+        alert("Por favor, escribí un correo electrónico válido.");
+        mail.classList.add("is-invalid");
+        mail.focus();
+        return;
+    }
+
+    // Verifico si está ingresado al menos 1 ticket, sino se detiene
+    if ( (cantidadTickets.value == 0) || (isNaN(cantidadTickets.value)) ) {
+        alert("Por favor, ingresá correctamente cantidad de tickets.");
+        cantidadTickets.classList.add("is-invalid");
+        cantidadTickets.focus();
+        return;
+    }
+
+    // Verifico que haya seleccionado una categoría, sino se detiene
+    if (categoria.value == "") {
+        alert("Por favor, seleccioná una categoría.");
+        categoria.classList.add("is-invalid");
+        categoria.focus();
+        return;
+    }
+
+    // Multiplico cantidad de tickets por el valor
+    let totalValorTickets = (cantidadTickets.value) * valorTicket;
+
+    // Aplico descuentos
+    if (categoria.value == 0) {
+        totalValorTickets = totalValorTickets ;
+    }
+    if (categoria.value == 1) {
+        totalValorTickets = totalValorTickets - (descuentoEstudiante / 100 * totalValorTickets);
+    }
+    if (categoria.value == 2) {
+        totalValorTickets = totalValorTickets - (descuentoTrainee / 100 * totalValorTickets);
+    }
+    if (categoria.value == 3) {
+        totalValorTickets = totalValorTickets - (descuentoJunior / 100 * totalValorTickets);
+    }
+
+    // Inserto el valor en el HTML
+    totalPago.innerHTML = totalValorTickets;
+}
+
+// Botón Resumen recibe un escuchador y la funcion del cálculo
+btnResumen.addEventListener('click', total_a_pagar);
+
+// Función para el botón Borrar
+function reset_total_a_pagar() {
+    quitarClaseError();
+    totalPago.innerHTML = "";
+}
+btnBorrar.addEventListener('click', reset_total_a_pagar);
